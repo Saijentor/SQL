46837bf67497ae2592c44eab0815bd5bf2695ff9
Saijentor <Saijentor@gmail.com>
2022-06-01 17:20:13 +0300

    Complete 5 tasks

--- sqlex.sql
+++ sqlex.sql
@@ -6,3 +6,15 @@ WHERE price < 500
 2)Найдите производителей принтеров. Вывести: maker
 SELECT DISTINCT maker FROM product
 WHERE type = 'printer'
+
+3)Найдите номер модели, объем памяти и размеры экранов ПК-блокнотов, цена которых превышает 1000 дол.:
+SELECT model,ram,screen FROM laptop
+WHERE price > 1000
+
+4)Найдите все записи таблицы Printer для цветных принтеров.:
+SELECT * FROM Printer
+WHERE color = 'y'
+
+5)Найдите номер модели, скорость и размер жесткого диска ПК, имеющих 12x или 24x CD и цену менее 600 дол.:
+SELECT model,speed,hd FROM pc
+WHERE (cd = '12x' OR cd ='24x') AND price < 600
