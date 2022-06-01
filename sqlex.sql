d09a66bb0ff234485644a2832274ebac2d176ffe
Saijentor <Saijentor@gmail.com>
2022-06-01 19:15:21 +0300

    update 5 task

--- sqlex.sql
+++ sqlex.sql
@@ -18,3 +18,6 @@ WHERE color = 'y'
 5)Найдите номер модели, скорость и размер жесткого диска ПК, имеющих 12x или 24x CD и цену менее 600 дол.:
 SELECT model,speed,hd FROM pc
 WHERE (cd = '12x' OR cd ='24x') AND price < 600
+or
+SELECT model,speed,hd FROM pc
+WHERE cd IN('12x','24x') AND price < 600
