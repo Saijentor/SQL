20224369d2d5be96c8b039e6ca1fb809cba67da8
Saijentor <Saijentor@gmail.com>
2022-06-21 17:46:48 +0300

    fix hw1,2

--- SQL HomeWork 2. Joins.sql
+++ SQL HomeWork 2. Joins.sql
@@ -37,35 +37,35 @@ order by role_name;
 select employees_name,role_name from employees e
 join roles_employee re on e.id=re.employee_id
 join roles r on re.role_id=r.id 
-where role_name like '% Java %developer'
+where role_name like '%Java developer%'
 order by role_name;
 
 --8. Вывести имена и должность только Python разработчиков.
 select employees_name,role_name from employees e
 join roles_employee re on e.id=re.employee_id
 join roles r on re.role_id=r.id 
-where role_name like '% Python %developer%'
+where role_name like '%Python developer%'
 order by role_name;
 
 --9. Вывести имена и должность всех QA инженеров.
 select employees_name,role_name from employees e
 join roles_employee re on e.id=re.employee_id
 join roles r on re.role_id=r.id 
-where role_name like '% QA %engineer%'
+where role_name like '%QA engineer%'
 order by role_name;
 
 --10. Вывести имена и должность ручных QA инженеров.
 select employees_name,role_name from employees e
 join roles_employee re on e.id=re.employee_id
 join roles r on re.role_id=r.id 
-where role_name like '% Manual QA%engineer%'
+where role_name like '%Manual QA engineer%'
 order by role_name;
 
 --11. Вывести имена и должность автоматизаторов QA
 select employees_name,role_name from employees e
 join roles_employee re on e.id=re.employee_id
 join roles r on re.role_id=r.id 
-where role_name like '% Automation QA%'
+where role_name like '%Automation QA%'
 order by role_name;
 
 -- 12. Вывести имена и зарплаты Junior специалистов
@@ -97,14 +97,14 @@ select monthly_salary from salary s
 join employee_salary es on s.id=es.salary_id 
 join roles_employee re on es.employee_id=re.employee_id 
 join roles r on re.role_id=r.id 
-where role_name like '% Java %developer';
+where role_name like '%Java developer%';
 
 --16. Вывести зарплаты Python разработчиков
 select monthly_salary from salary s 
 join employee_salary es on s.id=es.salary_id 
 join roles_employee re on es.employee_id=re.employee_id 
 join roles r on re.role_id=r.id 
-where role_name like '% Python %developer';
+where role_name like '%Python developer%';
 
 --17. Вывести имена и зарплаты Junior Python разработчиков
 select employees_name,monthly_salary from employee_salary es
@@ -112,7 +112,7 @@ join employees e on es.employee_id=e.id
 join salary s on es.salary_id=s.id
 join roles_employee re on re.employee_id=e.id
 join roles r on re.role_id=r.id
-where role_name like '%Junior Python%developer%';
+where role_name like '%Junior Python developer%';
 
 --18. Вывести имена и зарплаты Middle JS разработчиков
 select employees_name,monthly_salary from employee_salary es
@@ -120,7 +120,7 @@ join employees e on es.employee_id=e.id
 join salary s on es.salary_id=s.id
 join roles_employee re on re.employee_id=e.id
 join roles r on re.role_id=r.id
-where role_name like '%Middle JavaScript%developer%';
+where role_name like '%Middle JavaScript developer%';
 
 --19. Вывести имена и зарплаты Senior Java разработчиков
 select employees_name,monthly_salary from employee_salary es
@@ -128,7 +128,7 @@ join employees e on es.employee_id=e.id
 join salary s on es.salary_id=s.id
 join roles_employee re on re.employee_id=e.id
 join roles r on re.role_id=r.id
-where role_name like '%Senior Java %developer%';
+where role_name like '%Senior Java developer%';
 
 --20. Вывести зарплаты Junior QA инженеров
 select employees_name,monthly_salary from employee_salary es
@@ -136,7 +136,7 @@ join employees e on es.employee_id=e.id
 join salary s on es.salary_id=s.id
 join roles_employee re on re.employee_id=e.id
 join roles r on re.role_id=r.id
-where role_name like '%Junior%QA%engineer%';
+where role_name like '%Junior%QA engineer%';
 
 --21. Вывести среднюю зарплату всех Junior специалистов
 select avg(monthly_salary) avg_salary_junior from salary s 
@@ -147,18 +147,16 @@ join roles r on r.id=re.role_id
 where role_name like '%Junior%';
 
 --22. Вывести сумму зарплат JS разработчиков
-select avg(monthly_salary) avg_salary_js from salary s 
+select sum(monthly_salary) avg_salary_js from salary s 
 join employee_salary es on s.id=es.salary_id 
-join employees e on e.id=es.employee_id 
-join roles_employee re on re.employee_id=e.id 
+join roles_employee re on re.employee_id=es.id 
 join roles r on r.id=re.role_id 
 where role_name like '%JavaScript%developer';
 
 --23. Вывести минимальную ЗП QA инженеров
 select min(monthly_salary) min_salary_QA_engineer from salary s 
 join employee_salary es on s.id=es.salary_id 
-join employees e on e.id=es.employee_id 
-join roles_employee re on re.employee_id=e.id 
+join roles_employee re on re.employee_id=es.id 
 join roles r on r.id=re.role_id 
 where role_name like '%%QA%engineer%';
 
@@ -194,11 +192,12 @@ where role_name like '%developer%';
 
 --29. Вывести имена, должности и ЗП всех специалистов по возрастанию
 select employees_name,role_name,monthly_salary from employees e 
-left join roles_employee re on e.id=re.employee_id 
+left join roles_employee
+re on e.id=re.employee_id 
 left join roles r on r.id=re.role_id 
 left join employee_salary es on re.id=es.employee_id 
 left join salary s on es.salary_id=s.id
-order by monthly_salary;
+order by monthly_salary,role_name;
 
 /*30. Вывести имена, должности и ЗП всех специалистов по возрастанию 
  * у специалистов у которых ЗП от 1700 до 2300*/
@@ -208,7 +207,7 @@ left join roles r on r.id=re.role_id
 left join employee_salary es on re.id=es.employee_id 
 left join salary s on es.salary_id=s.id
 where monthly_salary between 1700 and 2300
-order by monthly_salary;
+order by monthly_salary,role_name;
 
 /*31. Вывести имена, должности и ЗП всех специалистов по возрастанию 
  * у специалистов у которых ЗП меньше 2300*/
@@ -218,7 +217,7 @@ left join roles r on r.id=re.role_id
 left join employee_salary es on re.id=es.employee_id 
 left join salary s on es.salary_id=s.id
 where monthly_salary < 2300 or monthly_salary is null
-order by monthly_salary;
+order by monthly_salary,role_name;
 
 /*32. Вывести имена, должности и ЗП всех специалистов по возрастанию 
  *у специалистов у которых ЗП равна 1100, 1500, 2000*/
