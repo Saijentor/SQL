85c31aca721b97ee8bf2ebbaad63b0bf107c0bb2
Sergei Gavrilenko <102379036+Saijentor@users.noreply.github.com>
2022-07-09 15:31:51 +0300

    Update SQL_HW_1.sql

--- SQL_HW_1.sql
+++ SQL_HW_1.sql
@@ -1,85 +1,85 @@
--- 1. Вывести все поля и все строки. ||  2. Вывести всех студентов в таблице
+--1. Display all fields and all lines. || 2. Display all students in the table:
 select * from students;
--- 3. Вывести только Id пользователей
+-- 3. Display only user IDs:
 select id from students;
--- 4. Вывести только имя пользователей
+-- 4. Display only the username:
 select name from students;
--- 5. Вывести только email пользователей
+-- 5. Display only user emails:
 select email from students;
--- 6. Вывести имя и email пользователей
+-- 6. Display username and email:
 select name,email from students;
--- 7. Вывести id, имя, email и дату создания пользователей
-select id,name,email,created_on  from students;
--- 8. Вывести пользователей где password 12333
-select *  from students
+-- 7. Display id, name, email and creation date of users:
+select id,name,email,created_on from students;
+-- 8. Display users where password is 12333:
+select * from students
 where password = '12333';
--- 9. Вывести пользователей которые были созданы 2021-03-26 00:00:00
-select *  from students
+-- 9. Display users who were created 2021-03-26 00:00:00:
+select * from students
 where created_on = '2021-03-26 00:00:00';
--- 10. Вывести пользователей где в имени есть слово Анна
-select *  from students
+-- 10. Display users where the name contains the word Anna:
+select * from students
 where name like '%Anna%';
--- 11. Вывести пользователей где в имени в конце есть 8
-select *  from students
+-- 11. Display users where the name ends with 8:
+select * from students
 where name like '%8';
--- 12. Вывести пользователей где в имени в есть буква а
-select *  from students
+-- 12. Display users where there is a letter a in their name:
+select * from students
 where name like '%a%';
--- 13. Вывести пользователей которые были созданы 2021-07-12 00:00:00
-select *  from students
+-- 13. Display users who were created 2021-07-12 00:00:00:
+select * from students
 where created_on = '2021-07-12 00:00:00';
--- 14. Вывести пользователей которые были созданы 2021-07-12 00:00:00 и имеют пароль 1m313
-select *  from students
+-- 14. Display users who were created on 2021-07-12 00:00:00 and have a password of 1m313:
+select * from students
 where created_on = '2021-07-12 00:00:00' and password = '1m313';
--- 15. Вывести пользователей которые были созданы 2021-07-12 00:00:00 и у которых в имени есть слово Andrey
-select *  from students
+-- 15. Display users who were created on 2021-07-12 00:00:00 and have the word Andrey in their name:
+select * from students
 where created_on = '2021-07-12 00:00:00' and name like '%Andrey%';
--- 16. Вывести пользователей которые были созданы 2021-07-12 00:00:00 и у которых в имени есть цифра 8
-select *  from students
+-- 16. Display users who were created on 2021-07-12 00:00:00 and have the number 8 in their name:
+select * from students
 where created_on = '2021-07-12 00:00:00' and name like '%8%';
--- 17. Вывести пользователя у которых id равен 110
-select *  from students
+-- 17. Display user whose id is 110:
+select * from students
 where id = 110
 order by id;
--- 18. Вывести пользователя у которых id равен 153
-select *  from students
+-- 18. Display user whose id is equal to 153:
+select * from students
 where id = 153;
--- 19. Вывести пользователя у которых id больше 140
-select *  from students
+-- 19. Display user whose id is greater than 140:
+select * from students
 where id > 140;
--- 20. Вывести пользователя у которых id меньше 130
-select *  from students
+-- 20. Display user whose id is less than 130:
+select * from students
 where id < 130;
--- 21. Вывести пользователя у которых id меньше 127 или больше 188
-select *  from students
+-- 21. Display user whose id is less than 127 or greater than 188:
+select * from students
 where id < 127 or id > 188;
--- 22. Вывести пользователя у которых id меньше либо равно 137
-select *  from students
+-- 22. Display user whose id is less than or equal to 137:
+select * from students
 where id <= 137;
--- 23. Вывести пользователя у которых id больше либо равно 137
-select *  from students
+-- 23. Display user whose id is greater than or equal to 137:
+select * from students
 where id >= 137;
--- 24. Вывести пользователя у которых id больше 180 но меньше 190
-select *  from students
+-- 24. Display user whose id is greater than 180 but less than 190:
+select * from students
 where id > 180 and id < 190;
--- 25. Вывести пользователя у которых id между 180 и 190
-select *  from students
+-- 25. Display user whose id is between 180 and 190:
+select * from students
 where id between 180 and 190;
--- 26. Вывести пользователей где password равен 12333, 1m313, 123313
-select *  from students
+-- 26. Display users where password is 12333, 1m313, 123313:
+select * from students
 where password in('12333','1m313','123313');
--- 27. Вывести пользователей где created_on равен 2020-10-03 00:00:00, 2021-05-19 00:00:00, 2021-03-26 00:00:00
-select *  from students
+-- 27. Display users where created_on is 2020-10-03 00:00:00, 2021-05-19 00:00:00, 2021-03-26 00:00:00:
+select * from students
 where created_on in('2020-10-03 00:00:00', '2021-05-19 00:00:00', '2021-03-26 00:00:00');
--- 28. Вывести минимальный id 
+-- 28. Display the minimum id:
 select min(id) from students;
--- 29. Вывести максимальный.
+-- 29. Display the maximum:
 select max(id) from students;
--- 30. Вывести количество пользователей
+-- 30. Display number of users:
 select count(id) from students;
--- 31. Вывести id пользователя, имя, дату создания пользователя. Отсортировать по порядку возрастания даты добавления пользоватлеля.
+-- 31. Display user id, name, user creation date. Sort in ascending order by the date the user was added:
 select id,name,created_on from students
 order by created_on ASC;
--- 32. Вывести id пользователя, имя, дату создания пользователя. Отсортировать по порядку убывания даты добавления пользоватлеля.
+-- 32. Display user id, name, user creation date. Sort in descending order by the date the user was added:
 select id,name,created_on from students
 order by created_on DESC;
